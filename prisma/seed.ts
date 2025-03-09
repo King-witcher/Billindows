c3bf39fa00ece34f1a319fad796d6820b8a844fa
Giuseppe Lanna <giuseppe.2000@hotmail.com>
2025-03-09 16:10:05 -0300

    update schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,44 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { range } from 'lodash'
+import { faker } from '@faker-js/faker'
 
 const prisma = new PrismaClient()
 
-async function main() {}
+function createCategory(): Prisma.CategoryCreateWithoutUserInput {
+  return {
+    name: faker.word.noun(),
+    color: faker.color.rgb(),
+    goal: faker.number.float({ min: 400, max: 2000 }),
+  }
+}
+
+function createUser(): Prisma.UserCreateInput {
+  return {
+    name: faker.person.fullName(),
+    firebaseId: faker.string.alphanumeric(28),
+  }
+}
+
+async function main() {
+  console.log('Seeding users...')
+  const users = await prisma.user.createManyAndReturn({
+    data: range(20).map(createUser),
+  })
+
+  console.log('Seeding categories...')
+  for (const user of users) {
+    const categories = range(20).map(createCategory)
+    console.log(user.id)
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        categories: {
+          create: categories,
+        },
+      },
+    })
+  }
+}
 
 main()
   .catch(console.error)
