5c198f67e871595d4f1063a565f69cadc85f402b
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-01-11 21:57:22 -0300

    update assistant icon

--- src/components/organisms/assistant-button/assistant-button.tsx
+++ src/components/organisms/assistant-button/assistant-button.tsx
@@ -6,13 +6,14 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 import { ChatWindow } from './chat-window'
+import { BotIcon, Brain, BrainCircuit } from 'lucide-react'
 
 export function AssistantButton() {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <div className="fixed bottom-8 right-8 z-10 rounded-[9999px] px-6 py-3 text-white border-none shadow-md cursor-pointer font-semibold bg-blue-500">
-          Assistente
+        <div className="fixed bottom-8 right-8 z-10 rounded-[9999px] size-14 flex items-center justify-center text-white border-none shadow-lg cursor-pointer font-semibold bg-blue-500">
+          <BotIcon size={28} />
         </div>
       </PopoverTrigger>
       <PopoverContent side="top" align="end" className="w-100 h-130 p-0">
