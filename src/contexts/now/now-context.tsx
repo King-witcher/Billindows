5eeea85fffa08447f5483b2601dd3849d002fc8d
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-13 20:33:31 -0300

    create a global time state

---
+++ src/contexts/now/now-context.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { createContext, use, useMemo } from 'react'
+
+export type NowData = {
+  now: Date
+  year: number
+  /** Month from 1 to 12 */
+  month: number
+  day: number
+}
+
+const NowContext = createContext<NowData | null>(null)
+
+export function NowProvider({ children }: { children: React.ReactNode }) {
+  const now = useMemo(() => new Date(), [])
+
+  const value = useMemo(
+    () => ({
+      now,
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate(),
+    }),
+    [now],
+  )
+
+  return <NowContext value={value}>{children}</NowContext>
+}
+
+export function useNow(): NowData {
+  const ctx = use(NowContext)
+
+  if (!ctx) {
+    throw new Error('useTime must be used within a TimeProvider')
+  }
+
+  return ctx
+}
