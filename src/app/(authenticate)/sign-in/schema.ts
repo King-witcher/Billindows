758331acc7086e6aff4883e6a498bf0a89abe168
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-13 20:35:27 -0300

    refactor login pages

--- src/app/(authenticate)/sign-in/schema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod'
-
-export const schema = z.object({
-  email: z.email().nonempty('Email is required'),
-  password: z.string(),
-  referrer: z.string().default('/'),
-})
-
-export type SignInPayload = z.infer<typeof schema>
