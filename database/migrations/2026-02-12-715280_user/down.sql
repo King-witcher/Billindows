517447e1d4b204f863c2e8b8cb329ef1101ecbd9
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-12 23:40:37 -0300

    remove drop index

--- database/migrations/2026-02-12-715280_user/down.sql
+++ database/migrations/2026-02-12-715280_user/down.sql
@@ -1,4 +1,3 @@
 -- Migration generated on 2026-02-12 19:52:8
 
-DROP INDEX "user_email_idx";
 DROP TABLE "user";
--- database/migrations/2026-02-12-719152_category/down.sql
+++ database/migrations/2026-02-12-719152_category/down.sql
@@ -1,4 +1,3 @@
 -- Migration generated on 2026-02-12 19:58:35
 
-DROP INDEX "category_user_id_idx";
 DROP TABLE "category";
